4e60d92abb6de75cecd34eebe839cdea55cbabcc
George Chiramattel <georgeck@gmail.com>
2025-04-05 21:32:27 -0700

    minor code formatting

--- client.js
+++ client.js
@@ -9,12 +9,13 @@ const app = initializeApp(config);
 const database = getDatabase(app);
 // const user = ref(database, "v0/items/jl");
 const item = ref(database, "v0/maxitem");
+
 onValue(item, async (snapshot) => {
     const itemID = snapshot.val();
     console.log(itemID);
     await sleep(1000);
     fetch("https://hacker-news.firebaseio.com/v0/item/" + itemID + ".json")
-        .then( resp =>  resp.json())
+        .then(resp => resp.json())
         .then(jsonData => console.log(jsonData))
 });
 
